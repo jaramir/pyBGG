819edaad3d7171a61ab2207a13d7cd4f94072317
Francesco Gigli <jaramir@gmail.com>
2012-02-23 16:35:36 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name         = 'pyBGG',
+    version      = '0.1',
+    description  = 'BoardGameGeek API for Python',
+    author       = 'Francesco Gigli',
+    author_email = 'jaramir@gmail.com',
+    packages     = find_packages(),
+)
