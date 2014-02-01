fb1cbc71d741057ff3d7a5e1b596d321942d0daf
Francesco Gigli <jaramir@gmail.com>
2014-02-01 03:05:41 +0000

    Added support for GeekList fetching

--- pyBGG/__init__.py
+++ pyBGG/__init__.py
@@ -29,70 +29,70 @@ root = "http://www.boardgamegeek.com/xmlapi/"
 
 boardgame_cache = {}
 
-class BoardGame( object ):
-    def __init__( self, et, fetched=False ):
+class BoardGame(object):
+    def __init__(self, et, fetched=False):
         self.et = et
         self.id = et.attrib["objectid"]
         self.fetched = fetched
 
-    def __getattr__( self, name ):
-        el = self.__find( name )
+    def __getattr__(self, name):
+        el = self.__find(name)
         if el is None:
-            raise AttributeError( name )
+            raise AttributeError(name)
         return el.text
 
-    def __fetch( self ):
+    def __fetch(self):
         if self.fetched:
             return
 
         if self.id not in boardgame_cache:
-            url = root + "boardgame/" + urllib.parse.quote( self.id )
-            tree = ET.parse( urllib.request.urlopen( url ) )
-            boardgame_cache[self.id] = tree.find( "boardgame" )
+            url = root + "boardgame/" + urllib.parse.quote(self.id)
+            tree = ET.parse(urllib.request.urlopen(url))
+            boardgame_cache[self.id] = tree.find("boardgame")
         self.et = boardgame_cache[self.id]
 
         self.fetched = True
 
-    def __find( self, name ):
-        el = self.et.find( name )
+    def __find(self, name):
+        el = self.et.find(name)
         if el is None:
             self.__fetch()
-            el = self.et.find( name )
+            el = self.et.find(name)
         return el
 
-    def __findall( self, term ):
-        names = self.et.findall( term )
+    def __findall(self, term):
+        names = self.et.findall(term)
         if not names:
             self.__fetch()
-            names = self.et.findall( term )
+            names = self.et.findall(term)
         return names
 
-    def __eq__( self, other ):
-        return isinstance( other, self.__class__ ) and self.id == other.id
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.id == other.id
 
-    def __ne__( self, other ):
-        return not self.__eq__( other )
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     @classmethod
-    def by_id( cls, id ):
+    def by_id(cls, id):
         """
         Creates a BoardGame instance given an objectid (accessible via the id attribute).
 
         """
-        et = ET.fromstring( '<boardgame objectid="%s"/>' % id )
-        return cls( et )
+        et = ET.fromstring('<boardgame objectid="%s"/>' % id)
+        return cls(et)
 
     @property
-    def name( self ):
+    def name(self):
         """
         Returns the first name.
         Prefear primary names.
 
         """
-        names = self.__findall( "name" )
+        names = self.__findall("name")
 
         # sort by sortindex if available
-        names.sort( key=lambda e: e.attrib.get( "sortindex", "1" ) )
+        names.sort(key=lambda e: e.attrib.get("sortindex", "1"))
 
         # find a primary one
         for name in names:
@@ -103,41 +103,41 @@ class BoardGame( object ):
         return names[0].text
 
     @property
-    def names( self ):
+    def names(self):
         """
         Returns all names sorted by sortindex (popularity, I suppose).
 
         """
         self.__fetch() # requires full info
-        names = self.__findall( "name" )
-        names.sort( key= lambda e: e.attrib["sortindex"] )
+        names = self.__findall("name")
+        names.sort(key= lambda e: e.attrib["sortindex"])
         return [ n.text for n in names ]
 
     @property
-    def thumbnail( self ):
+    def thumbnail(self):
         """
         Returns the thumbnail.
 
         """
         self.__fetch() # requires full info
-        thumbnail = self.et.find( "thumbnail" )
+        thumbnail = self.et.find("thumbnail")
         if thumbnail is not None:
             return thumbnail.text
         return None
 
     @property
-    def image( self ):
+    def image(self):
         """
         Returns the image.
 
         """
         self.__fetch() # requires full info
-        image = self.et.find( "image" )
+        image = self.et.find("image")
         if image is not None:
             return image.text
         return None
 
-def search( term, exact=False, prefetch=False ):
+def search(term, exact=False, prefetch=False):
     """
     Search for games by name and by AKAs and return a list of BoardGames
 
@@ -147,20 +147,20 @@ def search( term, exact=False, prefetch=False ):
 
     """
 
-    url = root + "search?search=" + urllib.parse.quote( term )
+    url = root + "search?search=" + urllib.parse.quote(term)
     if exact:
         url += "&exact=1"
 
-    tree = ET.parse( urllib.request.urlopen( url ) )
+    tree = ET.parse(urllib.request.urlopen(url))
 
-    games = tree.findall( "boardgame" )
+    games = tree.findall("boardgame")
 
     if prefetch:
-        games = __prefetch( [ urllib.parse.quote( el.attrib["objectid"] ) for el in games ] )
+        games = __prefetch([ urllib.parse.quote(el.attrib["objectid"]) for el in games ])
 
     rv = []
     for bg in games:
-        rv.append( BoardGame( bg, fetched=prefetch ) )
+        rv.append(BoardGame(bg, fetched=prefetch))
 
     if exact:
         if rv:
@@ -168,7 +168,7 @@ def search( term, exact=False, prefetch=False ):
         return None
     return rv
 
-def collection( username, own=False, prefetch=False ):
+def collection(username, own=False, prefetch=False):
     """
     Retrieve games in a user's collection.
 
@@ -178,29 +178,41 @@ def collection( username, own=False, prefetch=False ):
 
     """
 
-    url = root + "collection/" + urllib.parse.quote( username )
+    url = root + "collection/" + urllib.parse.quote(username)
     if own:
         url += "?own=1"
 
-    tree = ET.parse( urllib.request.urlopen( url ) )
+    return __fetch_games(url, prefetch)
 
-    games = tree.findall( "item[@objecttype='thing'][@subtype='boardgame']" )
+def geeklist(list_id, prefetch=False):
+    """
+    Retrieve all boardgames contained into a GeekList
+
+    """
+    url = root + "geeklist/" + urllib.parse.quote(list_id)
+
+    return __fetch_games(url, prefetch)
+
+def __fetch_games(url, prefetch=False):
+    tree = ET.parse(urllib.request.urlopen(url))
+
+    games = tree.findall("item[@objecttype='thing'][@subtype='boardgame']")
 
     if prefetch:
-        games = __prefetch( [ urllib.parse.quote( el.attrib["objectid"] ) for el in games ] )
+        games = __prefetch([ urllib.parse.quote(el.attrib["objectid"]) for el in games ])
 
     rv = []
     for bg in games:
-        rv.append( BoardGame( bg, fetched=prefetch ) )
+        rv.append(BoardGame(bg, fetched=prefetch))
     return rv
 
-def __prefetch( ids ):
+def __prefetch(ids):
     """
     Fetch and push in boardgame_cache full info for all boardgames ids passed
 
     """
-    murl = root + "boardgame/" + ",".join( ids )
-    mtree = ET.parse( urllib.request.urlopen( murl ) )
-    for subtree in mtree.findall( "boardgame" ):
+    murl = root + "boardgame/" + ",".join(ids)
+    mtree = ET.parse(urllib.request.urlopen(murl))
+    for subtree in mtree.findall("boardgame"):
         boardgame_cache[subtree.attrib["objectid"]] = subtree
         yield subtree
--- test.py
+++ test.py
@@ -25,190 +25,221 @@ import pyBGG
 
 # mock BGG server
 
-def read_fixture( name ):
-    with open( "fixture/" + name ) as fp:
+def read_fixture(name):
+    with open("fixture/" + name) as fp:
         return fp.read()
 
+
+
 canned_response = {
-    "http://www.boardgamegeek.com/xmlapi/boardgame/13": read_fixture( "bgCatan" ),
-    "http://www.boardgamegeek.com/xmlapi/boardgame/421": read_fixture( "bg1830" ),
-    "http://www.boardgamegeek.com/xmlapi/boardgame/66000,68448,31260,43018,2346,58110,2452,34010,60054,24480,19764,8192,20551,40692,57072,57070,71818,27627,34127,20426,11955,28025": read_fixture( "bgMulti" ),
-    "http://www.boardgamegeek.com/xmlapi/search?search=1830%3A%20Railways%20%26%20Robber%20Barons": read_fixture( "search1830name" ),
-    "http://www.boardgamegeek.com/xmlapi/search?search=I%20Coloni%20di%20Catan&exact=1": read_fixture( "searchCatan" ),
-    "http://www.boardgamegeek.com/xmlapi/search?search=pyBGG%20test%20search&exact=1": read_fixture( "searchTest" ),
-    "http://www.boardgamegeek.com/xmlapi/collection/cesco": read_fixture( "collectionCesco" ),
-    "http://www.boardgamegeek.com/xmlapi/collection/cesco?own=1": read_fixture( "collectionCescoOwn" ),
-    "http://www.boardgamegeek.com/xmlapi/search?search=1830": read_fixture( "search1830" ),
-    "http://www.boardgamegeek.com/xmlapi/boardgame/88400,37322,31013,111775,421,31230,56839,56841,56840,59644,59645,23189,70875,2396": read_fixture( "bgMulti1830" ),
-    "http://www.boardgamegeek.com/xmlapi/collection/Iago71?own=1": read_fixture( "collectionIago71" ),
-    "http://www.boardgamegeek.com/xmlapi/boardgame/111377": read_fixture( "bgNoImages" ),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/13": read_fixture("bgCatan"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/421": read_fixture("bg1830"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/66000,68448,31260,43018,2346,58110," +
+        "2452,34010,60054,24480,19764,8192,20551,40692,57072,57070,71818,27627,34127," +
+        "20426,11955,28025": read_fixture("bgMulti"),
+    "http://www.boardgamegeek.com/xmlapi/search?search=1830" +
+        "%3A%20Railways%20%26%20Robber%20Barons": read_fixture("search1830name"),
+    "http://www.boardgamegeek.com/xmlapi/search?search=I%20Coloni%20di%20Catan&exact=1": read_fixture("searchCatan"),
+    "http://www.boardgamegeek.com/xmlapi/search?search=pyBGG%20test%20search&exact=1": read_fixture("searchTest"),
+    "http://www.boardgamegeek.com/xmlapi/collection/cesco": read_fixture("collectionCesco"),
+    "http://www.boardgamegeek.com/xmlapi/collection/cesco?own=1": read_fixture("collectionCescoOwn"),
+    "http://www.boardgamegeek.com/xmlapi/search?search=1830": read_fixture("search1830"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/88400,37322,31013,111775,421,31230,56839," +
+        "56841,56840,59644,59645,23189,70875,2396": read_fixture("bgMulti1830"),
+    "http://www.boardgamegeek.com/xmlapi/collection/Iago71?own=1": read_fixture("collectionIago71"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/111377": read_fixture("bgNoImages"),
+    "http://www.boardgamegeek.com/xmlapi/geeklist/72020": read_fixture("geeklist"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/91313,2397,315,16826,372,2453,1293,11733,24264," +
+        "41,822,2582,171,521,86775,20253,36218,4031,4386,3181,14538,88563,25574,188,38950,5451,2759," +
+        "4112,8203,2932,17114,6862,21488,43413,8909,2448,590,18901,29146,1295,2389,1419,13,503,1111," +
+        "681,8217,320,16395,9674,100423,29741,2921,483,67453,463,41021,30009,2223,1406,215,91034,86156," +
+        "2136,2653,1258,69638,2093,11646,2405,20240,1116,632,15889,6819,592,6887,19957,7854,2398,6901," +
+        "17104,4583,2181,18011,4505,17530,2093,949,2452,68251,21241,68188,2393,11640,6644,5,112118,93164," +
+        "181,4616,1198,7048,3406,38975,88147,2163,925,65990,36553,11130,27710,2065,117869,811,40692,19803," +
+        "71061,30,57037,41627,2952,1269,526,42,699,1644,714,2083,9220,9209,20545,463,2655,123185,22392," +
+        "10997,40943,624,25669,72287,1827,107529,6249,925,72667,131260,40692,19841,28396,40398,1917," +
+        "135220,113324,1219,21763": read_fixture("geeklistGames"),
+    "http://www.boardgamegeek.com/xmlapi/boardgame/2397": read_fixture("bgBackgammon")
 }
 
-class TestResponse( object ):
-    def __init__( self, request ):
+class TestResponse(object):
+    def __init__(self, request):
         url = request.get_full_url()
         if not url in canned_response:
-            raise Exception( "%s not in canned response!" % url )
+            raise Exception("%s not in canned response!" % url)
         self.request = request
         self.code = 200
         self.msg = "OK"
-        self.content = io.StringIO( canned_response[url] )
+        self.content = io.StringIO(canned_response[url])
         self.read = self.content.read
 
-    def info( self ):
+    def info(self):
         return []
 
-class TestHandler( urllib.request.HTTPHandler ):
-    def __init__( self ):
+class TestHandler(urllib.request.HTTPHandler):
+    def __init__(self):
         self.hits = []
 
-    def http_open( self, req ):
-        self.hits.append( req.get_full_url() )
-        return TestResponse( req )
+    def http_open(self, req):
+        self.hits.append(req.get_full_url())
+        return TestResponse(req)
 
-    def reset_hits( self ):
+    def reset_hits(self):
         self.hits = []
 
 handler = TestHandler()
-opener = urllib.request.build_opener( handler )
-urllib.request.install_opener( opener )
+opener = urllib.request.build_opener(handler)
+urllib.request.install_opener(opener)
 
 ## This works but will make a request per fixture on every run
 ## Uncomment and use when necessary
 ##
-##class fixtureTest( unittest.TestCase ):
-##    def test_fixtures( self ):
+##class fixtureTest(unittest.TestCase):
+##    def test_fixtures(self):
 ##        for url in canned_response.keys():
-##            req = urllib2.Request( url )
-##            op = urllib2.build_opener( urllib2.HTTPHandler )
-##            self.assertEqual( op.open( req ).read(), opener.open( req ).read() )
+##            req = urllib2.Request(url)
+##            op = urllib2.build_opener(urllib2.HTTPHandler)
+##            self.assertEqual(op.open(req).read(), opener.open(req).read())
 
-class handlerTest( unittest.TestCase ):
-    def setUp( self ):
+class handlerTest(unittest.TestCase):
+    def setUp(self):
         handler.reset_hits()
 
-    def test_count_hits( self ):
+    def test_count_hits(self):
         url = "http://www.boardgamegeek.com/xmlapi/boardgame/13"
-        urllib.request.urlopen( url ).read()
-        self.assertEqual( handler.hits, [url] )
+        urllib.request.urlopen(url).read()
+        self.assertEqual(handler.hits, [url])
 
-class pyBGGTest( unittest.TestCase ):
+class pyBGGTest(unittest.TestCase):
 
-    def setUp( self ):
+    def setUp(self):
         handler.reset_hits()
         pyBGG.boardgame_cache = {}
 
-    def test_exact_search_gone_wrong( self ):
-        bg = pyBGG.search( "pyBGG test search", exact=1 )
-        self.assertEqual( None, bg )
+    def test_exact_search_gone_wrong(self):
+        bg = pyBGG.search("pyBGG test search", exact=1)
+        self.assertEqual(None, bg)
 
-    def test_exact_search( self ):
-        bg = pyBGG.search( "I Coloni di Catan", exact=1 )
-        self.assertEqual( "13", bg.id )
+    def test_exact_search(self):
+        bg = pyBGG.search("I Coloni di Catan", exact=1)
+        self.assertEqual("13", bg.id)
 
-    def test_name_in_search( self ):
-        resp = pyBGG.search( "1830: Railways & Robber Barons" )
-        self.assertIn( "1830", resp[0].name )
+    def test_name_in_search(self):
+        resp = pyBGG.search("1830: Railways & Robber Barons")
+        self.assertIn("1830", resp[0].name)
 
-    def test_description_in_search( self ):
-        resp = pyBGG.search( "1830: Railways & Robber Barons" )
-        self.assertIn( "1830 is one of the most famous 18xx games", resp[0].description )
+    def test_description_in_search(self):
+        resp = pyBGG.search("1830: Railways & Robber Barons")
+        self.assertIn("1830 is one of the most famous 18xx games", resp[0].description)
 
-    def test_boardgame_by_id( self ):
-        bg = pyBGG.BoardGame.by_id( "421" )
-        self.assertEqual( "1830: Railways & Robber Barons", bg.name )
+    def test_boardgame_by_id(self):
+        bg = pyBGG.BoardGame.by_id("421")
+        self.assertEqual("1830: Railways & Robber Barons", bg.name)
 
-    def test_boardgame_info_taken_from_memory_cache( self ):
+    def test_boardgame_info_taken_from_memory_cache(self):
         pyBGG.boardgame_cache = {} # empty cache now
-        bg = pyBGG.BoardGame.by_id( "421" )
+        bg = pyBGG.BoardGame.by_id("421")
         bg.description
-        bg2 = pyBGG.BoardGame.by_id( "421" )
+        bg2 = pyBGG.BoardGame.by_id("421")
         bg2.names
-        self.assertEqual( handler.hits, [ "http://www.boardgamegeek.com/xmlapi/boardgame/421" ] )
+        self.assertEqual(handler.hits, [ "http://www.boardgamegeek.com/xmlapi/boardgame/421" ])
 
-    def test_game_with_many_sorted_names( self ):
+    def test_game_with_many_sorted_names(self):
         en = "The Settlers of Catan"
         us = "Catan"
         it = "I Coloni di Catan"
         ru = "Заселниците на Катан"
         ja = "カタンの開拓者"
 
-        bg = pyBGG.BoardGame.by_id( "13" )
+        bg = pyBGG.BoardGame.by_id("13")
         names = bg.names
 
-        self.assertIsInstance( names, list )
+        self.assertIsInstance(names, list)
 
-        self.assertIn( en, names ) # sort = 5
-        self.assertIn( us, names ) # sort = 1
-        self.assertIn( it, names ) # sort = 3
-        self.assertIn( ru, names ) # sort = 1
-        self.assertIn( ja, names ) # sort = 1
+        self.assertIn(en, names) # sort = 5
+        self.assertIn(us, names) # sort = 1
+        self.assertIn(it, names) # sort = 3
+        self.assertIn(ru, names) # sort = 1
+        self.assertIn(ja, names) # sort = 1
 
-        self.assertLess( names.index( ru ), names.index( it ), "Wrong sort: %s before %s" % ( it, ru ) )
-        self.assertLess( names.index( it ), names.index( en ), "Wrong sort: %s before %s" % ( en, it ) )
+        self.assertLess(names.index(ru), names.index(it), "Wrong sort: %s before %s" % (it, ru))
+        self.assertLess(names.index(it), names.index(en), "Wrong sort: %s before %s" % (en, it))
 
-    def test_game_names_from_search( self ):
-        bg = pyBGG.search( "I Coloni di Catan", exact=1 )
-        self.assertIn( "The Settlers of Catan", bg.names )
-        self.assertIn( "I Coloni di Catan", bg.names )
+    def test_game_names_from_search(self):
+        bg = pyBGG.search("I Coloni di Catan", exact=1)
+        self.assertIn("The Settlers of Catan", bg.names)
+        self.assertIn("I Coloni di Catan", bg.names)
 
-    def test_search_does_prefetch( self ):
-        games = pyBGG.search( "1830", prefetch=True )
+    def test_search_does_prefetch(self):
+        games = pyBGG.search("1830", prefetch=True)
         hits = [
             "http://www.boardgamegeek.com/xmlapi/search?search=1830",
             "http://www.boardgamegeek.com/xmlapi/boardgame/88400,37322,31013,111775,421,31230,56839,56841,56840,59644,59645,23189,70875,2396",
             ]
-        self.assertListEqual( handler.hits, hits )
+        self.assertListEqual(handler.hits, hits)
 
-    def test_collection( self ):
-        coll = pyBGG.collection( "cesco" )
-        self.assertIsInstance( coll, list )
-        self.assertIsInstance( coll[0], pyBGG.BoardGame )
-        self.assertIn( pyBGG.BoardGame.by_id( "57070" ), coll )
+    def test_collection(self):
+        coll = pyBGG.collection("cesco")
+        self.assertIsInstance(coll, list)
+        self.assertIsInstance(coll[0], pyBGG.BoardGame)
+        self.assertIn(pyBGG.BoardGame.by_id("57070"), coll)
 
-    def test_collection_prefetch( self ):
-        coll = pyBGG.collection( "cesco", own=True, prefetch=True )
+    def test_collection_prefetch(self):
+        coll = pyBGG.collection("cesco", own=True, prefetch=True)
         for game in coll:
             game.names # this would normaly generate a hit
         hits = [
             "http://www.boardgamegeek.com/xmlapi/collection/cesco?own=1",
             "http://www.boardgamegeek.com/xmlapi/boardgame/66000,68448,31260,43018,2346,58110,2452,34010,60054,24480,19764,8192,20551,40692,57072,57070,71818,27627,34127,20426,11955,28025",
             ]
-        self.assertListEqual( handler.hits, hits )
+        self.assertListEqual(handler.hits, hits)
 
-    def test_prefetch_populates_cache( self ):
-        coll = pyBGG.collection( "cesco", own=True, prefetch=True )
+    def test_prefetch_populates_cache(self):
+        coll = pyBGG.collection("cesco", own=True, prefetch=True)
         for game in coll:
-            self.assertIn( game.id, list(pyBGG.boardgame_cache.keys()) )
+            self.assertIn(game.id, list(pyBGG.boardgame_cache.keys()))
 
-    def test_collection_no_prefetch_no_requests( self ):
-        coll = pyBGG.collection( "Iago71", own=True, prefetch=False )
+    def test_collection_no_prefetch_no_requests(self):
+        coll = pyBGG.collection("Iago71", own=True, prefetch=False)
         for item in coll:
             item.name
-        self.assertListEqual( handler.hits, ["http://www.boardgamegeek.com/xmlapi/collection/Iago71?own=1"] )
+        self.assertListEqual(handler.hits, ["http://www.boardgamegeek.com/xmlapi/collection/Iago71?own=1"])
 
-    def test_no_thumbnail( self ):
-        bg = pyBGG.BoardGame.by_id( 111377 )
-        self.assertEqual( None, bg.thumbnail )
+    def test_no_thumbnail(self):
+        bg = pyBGG.BoardGame.by_id(111377)
+        self.assertEqual(None, bg.thumbnail)
 
-    def test_no_image( self ):
-        bg = pyBGG.BoardGame.by_id( 111377 )
-        self.assertEqual( None, bg.image )
+    def test_no_image(self):
+        bg = pyBGG.BoardGame.by_id(111377)
+        self.assertEqual(None, bg.image)
 
-    def test_no_useless_fetch( self ):
-        bg = pyBGG.BoardGame.by_id( 13 )
+    def test_no_useless_fetch(self):
+        bg = pyBGG.BoardGame.by_id(13)
         bg.names # does the fetch
         pyBGG.boardgame_cache = None # raise exception if hit
-        self.assertEqual( bg.image, "http://cf.geekdo-images.com/images/pic268839.jpg" )
-        self.assertEqual( bg.thumbnail, "http://cf.geekdo-images.com/images/pic268839_t.jpg" )
-        self.assertIsInstance( bg.names, list )
-        self.assertEqual( len( bg.names ), 40 )
+        self.assertEqual(bg.image, "http://cf.geekdo-images.com/images/pic268839.jpg")
+        self.assertEqual(bg.thumbnail, "http://cf.geekdo-images.com/images/pic268839_t.jpg")
+        self.assertIsInstance(bg.names, list)
+        self.assertEqual(len(bg.names), 40)
 
-    def test_prefetch_avoids_fetch( self ):
-        coll = pyBGG.collection( "cesco", own=True, prefetch=True )
+    def test_prefetch_avoids_fetch(self):
+        coll = pyBGG.collection("cesco", own=True, prefetch=True)
         pyBGG.boardgame_cache = None # raise exception if hit
         for bg in coll:
-            self.assertIsInstance( bg.names, list )
+            self.assertIsInstance(bg.names, list)
+
+    def test_geeklist(self):
+        geeklist = pyBGG.geeklist("72020")
+        self.assertIsInstance(geeklist, list)
+        self.assertIsInstance(geeklist[0], pyBGG.BoardGame)
+        self.assertIn(pyBGG.BoardGame.by_id("2397"), geeklist)
+        backgammon = geeklist[1]
+        self.assertEqual(backgammon.minplayers, "2")
+        self.assertEqual(backgammon.maxplayers, "2")
+        self.assertEqual(handler.hits, [
+            "http://www.boardgamegeek.com/xmlapi/geeklist/72020",
+            "http://www.boardgamegeek.com/xmlapi/boardgame/2397"
+            ])
 
 if __name__ == '__main__':
     unittest.main()
